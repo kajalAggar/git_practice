bc19defdbc0189ef452cddc62fd5e69f1f2acfc8
kajalAggar <45177135+kajalAggar@users.noreply.github.com>
2019-04-25 13:04:15 +0200

    Add files via upload

---
+++ GuessGame.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstdlib>
+
+
+void game()
+{
+	int max = 100;
+	int min = 0;
+	
+	srand(time(NULL)); // Seed the time
+	int finalNum = rand()%(max-min+1)+min; 
+
+	std::cout<<std::endl;
+	std::cout<<" ---------------------------------------------"<<std::endl;
+	std::cout<<" Hey Do you want to play guess a number game?"<<std::endl;
+	std::cout<<" Guess a number that is in my mind in 7 trails"<<std::endl;
+	std::cout<<" Dont worry, I will keep helping you"<<std::endl;
+	std::cout<<" The umber is between 0 and 100"<<std::endl;
+	std::cout<<" ---------------------------------------------"<<std::endl;
+	std::cout<<std::endl;
+
+
+	for (int i = 0; i<7; i++)
+	{
+					
+		std::cout<<"Enter guess n° "<<i+1<<std::endl;
+		int guess;
+		std::cin>>guess;
+		
+				
+
+		if (guess > finalNum)
+		{
+			std::cout<<"Your guess is higher than my number"<<std::endl;	
+			std::cout<<std::endl;			
+			std::cout<<"Ready for next guess?"<<std::endl;
+						
+		}	
+		else if (guess < finalNum)
+		{
+			std::cout<<"Your guess is lower than my number"<<std::endl;	
+			std::cout<<std::endl;			
+			std::cout<<"Ready for next guess?"<<std::endl;	
+					
+		}
+		else if (guess == finalNum)
+		{
+			std::cout<<"Congratulations.. you are soo cool"<<std::endl;	
+			std::cout<<"I lost! you won"<<std::endl;
+			break;			
+		}
+	}	
+	
+}
+
+bool toContinue()
+{
+	std::cout<<std::endl;	
+	std::cout<<"Do you want to play anoter round (y/n)"<<std::endl;
+	char c;
+	std::cin>>c;
+	if(c == 'y')
+	game();
+	else if (c == 'n')
+	return(1);
+
+}
+
+
+int main()
+{
+	game();
+	toContinue();
+	return 0;
+}
